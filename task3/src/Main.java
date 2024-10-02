75570028b5f374f39635e5973a23c9bd0cef282d
akerk <akerkeolzhatay@gmail.com>
2024-10-02 18:35:39 +0500

    task3

---
+++ task3/src/Main.java
@@ -0,0 +1,13 @@
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        House house = new House.HouseBuilder()
+                .setNumRooms(3)
+                .setNumFloors(2)
+                .setHasPool(true)
+                .setHasGarage(false)
+                .build();
+
+        System.out.println(house);
+    }
+}
